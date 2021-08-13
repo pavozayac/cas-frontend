2f2decf3b89263d8d5819b3905ddaa6bce050fb2
Pablo Z <exaus.lol@gmail.com>
2021-08-13 23:25:06 +0200

    Added form for adding comments, need to hook it up to felte

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -105,6 +105,12 @@
     {#if commentsVisible}
     <div class="comment-section" transition:slide>
         <div class="comments-wrapper">
+            <div class="add-comment">
+                <form class="add-comment-form">
+                    <input class="add-comment-input" type="text" placeholder="Add your comment"/>
+                    <input class="add-comment-button" type="submit" value="Post" />
+                </form>
+            </div>
             {#each mock_comments as comment}
                 <div class="comment">
                     <div class="comment-profile">
@@ -164,6 +170,39 @@
 
     .comment-text {
         margin-left: 2rem;
+    }
+
+    .add-comment-form {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 1rem;
+    }
+
+    .add-comment-input {
+        width: 100%;
+        margin-right: 1rem;
+        outline: none;
+        border: 2px solid var(--bg-grey-lower);
+        box-sizing: border-box;
+        border-radius: 1rem;
+        padding: 1rem;
+        background: var(--bg-grey);
+    }
+
+    input::placeholder {
+        font-family: Rubik, sans-serif;
+    }
+
+    .add-comment-button {
+        font-family: Rubik, sans-serif;
+        outline: none;
+        border: none;
+        padding: 1rem;
+        background: var(--accent-blue);
+        border-radius: 1rem;
+        height: 100%;
+        color: white;
     }
 
     .top-widgets {

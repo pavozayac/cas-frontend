ab08029fda3d1f69e1860d434c4c42c103c6b116
Paweł Z <pavlzayac@gmail.com>
2022-03-11 20:16:20 +0100

    New https

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,6 +1,6 @@
 import type { Writable } from "svelte/store";
 
-export const backendUrl = 'http://api.ibcas.pl/'
+export const backendUrl = 'https://api.ibcas.pl/'
 
 export function readStore<T>(store: Writable<T>): T {
     let value;
